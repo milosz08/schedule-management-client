0c37c72c98cb9aebcb9a4c72fb9eab3ac2964e4e
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-04-05 23:58:44 +0200

    update copyright header template

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,8 +1,9 @@
-/* * Copyright (c) 2022 by MILOSZ GILGA <https://miloszgilga.pl> <https://github.com/Milosz08>
+/*
+ * Copyright (c) 2022 by MILOSZ GILGA <https://miloszgilga.pl> <https://github.com/Milosz08>
  * Silesian University of Technology | Politechnika Śląska
  *
  * File name | Nazwa pliku: polyfills.ts
- * Last modified | Ostatnia modyfikacja: 05/04/2022, 23:52
+ * Last modified | Ostatnia modyfikacja: 05/04/2022, 23:55
  * Project name | Nazwa Projektu: angular-po-schedule-management-client
  *
  * Klient | Client: <https://github.com/Milosz08/Angular_PO_Schedule_Management_Client>
@@ -12,7 +13,7 @@
  * for generating dynamic web applications. Project for the teaching course "Objected Oriented Programming".
  *
  * Klient dla aplikacji ASP.NET Core służącej do zarządzania planem zajęć uczelni. Napisany przy użyciu frameworka
- * Angular 2.0 do generowania dynamicznych aplikacji webowych. Projekt wykonany na zajęcia "Programowanie
+ * Angular do generowania dynamicznych aplikacji webowych. Projekt wykonany na zajęcia "Programowanie
  * Obiektowe".
  */
 
