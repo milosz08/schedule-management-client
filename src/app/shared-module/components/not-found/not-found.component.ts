2d307027b593b305b4b7f3a37259600d80103f3a
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-04-10 01:14:19 +0200

    change css to scss stylesheet

--- src/app/shared-module/components/not-found/not-found.component.ts
+++ src/app/shared-module/components/not-found/not-found.component.ts
@@ -22,6 +22,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-not-found',
     templateUrl: './not-found.component.html',
-    styleUrls: [ './not-found.component.css' ]
+    styleUrls: [ './not-found.component.scss' ]
 })
 export class NotFoundComponent {}
