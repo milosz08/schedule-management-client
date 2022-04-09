8d08751925535ebf796f45426dc09d4ff5845aec
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-04-10 00:58:23 +0200

    initialised login and admin protected routing angular guard

---
+++ src/app/guards/login-redirect.guard.ts
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2022 by MILOSZ GILGA <https://miloszgilga.pl> <https://github.com/Milosz08>
+ * Silesian University of Technology | Politechnika Śląska
+ *
+ * File name | Nazwa pliku: login-redirect.guard.ts
+ * Last modified | Ostatnia modyfikacja: 10/04/2022, 00:22
+ * Project name | Nazwa Projektu: angular-po-schedule-management-client
+ *
+ * Klient | Client: <https://github.com/Milosz08/Angular_PO_Schedule_Management_Client>
+ * Serwer | Server: <https://github.com/Milosz08/ASP.NET_PO_Schedule_Management_Server>
+ *
+ * Client for the ASP.NET Core application to manage schedule for sample university. Written with the Angular Framework
+ * for generating dynamic web applications. Project for the teaching course "Objected Oriented Programming".
+ *
+ * Klient dla aplikacji ASP.NET Core służącej do zarządzania planem zajęć uczelni. Napisany przy użyciu frameworka
+ * Angular do generowania dynamicznych aplikacji webowych. Projekt wykonany na zajęcia "Programowanie
+ * Obiektowe".
+ */
+
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+
+@Injectable({
+    providedIn: 'root',
+})
+export class LoginRedirectGuard implements CanActivate {
+
+    private isAuthenticated: boolean = true;
+
+    constructor(private router: Router) {};
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+        if (!this.isAuthenticated) {
+            return true;
+        }
+        this.router.navigate([ '/secure/admin-panel/dashboard' ]).then(r => r);
+        return false;
+    };
+}
