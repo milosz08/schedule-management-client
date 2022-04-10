9cc6369514701f5cbfb673b24215c04c9c545e63
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-04-10 14:45:53 +0200

    connect guards with ngrx store

--- src/app/guards/admin-redirect.guard.ts
+++ src/app/guards/admin-redirect.guard.ts
@@ -18,22 +18,35 @@
  */
 
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+
+import { Store } from '@ngrx/store';
+import { map, Observable } from 'rxjs';
+
+import { SessionStateKeysTypes } from '../store/types/session.types';
+import { InitialSessionStateTypes, sessionSelectSelector } from '../store/initial-state/session.initial';
+
 
 @Injectable({
     providedIn: 'root',
 })
 export class AdminRedirectGuard implements CanActivate {
 
-    private isAuthenticated: boolean = true;
+    private readonly ifLogged$: Observable<boolean>;
 
-    constructor(private router: Router) {};
+    constructor(
+        private router: Router,
+        private store: Store<InitialSessionStateTypes>
+    ) {
+        this.ifLogged$ = this.store.select(sessionSelectSelector(SessionStateKeysTypes.IF_LOGGED));
+    };
 
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-        if (this.isAuthenticated) {
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> {
+        return this.ifLogged$.pipe(map(authenticate => {
+            if (!authenticate) {
+                this.router.navigate([ '/schedule/login' ]).then(r => r);
+            }
             return true;
-        }
-        this.router.navigate([ '/schedule/login' ]).then(r => r);
-        return false;
+        }));
     };
 }
