1a7ba28056c5dea89e2ab8ec2184db1d862e9866
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-04-19 15:37:30 +0200

    create forgot-password angular page component

---
+++ src/app/modules/auth-module/pages/forgot-password-page/forgot-password-page.component.ts
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2022 by MILOSZ GILGA <https://miloszgilga.pl> <https://github.com/Milosz08>
+ * Silesian University of Technology | Politechnika Śląska
+ *
+ * File name | Nazwa pliku: forgot-password-page.component.ts
+ * Last modified | Ostatnia modyfikacja: 19/04/2022, 14:42
+ * Project name | Nazwa Projektu: angular-po-schedule-management-client
+ *
+ * Klient | Client: <https://github.com/Milosz08/Angular_PO_Schedule_Management_Client>
+ * Serwer | Server: <https://github.com/Milosz08/ASP.NET_PO_Schedule_Management_Server>
+ *
+ * Client for the ASP.NET Core application to manage schedule for sample university. Written with the Angular Framework
+ * for generating dynamic web applications. Project for the teaching course "Objected Oriented Programming".
+ *
+ * Klient dla aplikacji ASP.NET Core służącej do zarządzania planem zajęć uczelni. Napisany przy użyciu frameworka
+ * Angular do generowania dynamicznych aplikacji webowych. Projekt wykonany na zajęcia "Programowanie
+ * Obiektowe".
+ */
+
+import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+
+import { AllMainWebpages, MetaWebContent } from '../../../../utils/MetaWebContent';
+
+
+@Component({
+    selector: 'app-forgot-password-page',
+    templateUrl: './forgot-password-page.component.html',
+    styleUrls: [ './forgot-password-page.component.scss' ]
+})
+export class ForgotPasswordPageComponent extends MetaWebContent {
+
+    constructor(titleService: Title, metaService: Meta) {
+        super(titleService, metaService, AllMainWebpages.FORGOT_PASSWORD);
+    };
+}
