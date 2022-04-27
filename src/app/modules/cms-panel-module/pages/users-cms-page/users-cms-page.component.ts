b12ef3b03b2d14d17aced17aa62be4d1a5522157
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-04-27 23:36:36 +0200

    create users-cms-page angular component

---
+++ src/app/modules/cms-panel-module/pages/users-cms-page/users-cms-page.component.ts
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2022 by MILOSZ GILGA <https://miloszgilga.pl> <https://github.com/Milosz08>
+ * Silesian University of Technology | Politechnika Śląska
+ *
+ * File name | Nazwa pliku: users-cms-page.component.ts
+ * Last modified | Ostatnia modyfikacja: 27/04/2022, 10:13
+ * Project name | Nazwa Projektu: angular-po-schedule-management-client
+ *
+ * Klient | Client: <https://github.com/Milosz08/Angular_PO_Schedule_Management_Client>
+ * Serwer | Server: <https://github.com/Milosz08/ASP.NET_PO_Schedule_Management_Server>
+ *
+ * Client for the ASP.NET Core application to manage schedule for sample university. Written with the Angular Framework
+ * for generating dynamic web applications. Project for the teaching course "Objected Oriented Programming".
+ *
+ * Klient dla aplikacji ASP.NET Core służącej do zarządzania planem zajęć uczelni. Napisany przy użyciu frameworka
+ * Angular do generowania dynamicznych aplikacji webowych. Projekt wykonany na zajęcia "Programowanie
+ * Obiektowe".
+ */
+
+import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
+
+import { AllCmsWebpages, MetaWebContentHelper } from '../../../../utils/meta-web-content.helper';
+
+/**
+ *
+ */
+
+@Component({
+    selector: 'app-users-cms-page',
+    templateUrl: './users-cms-page.component.html',
+    styleUrls: [ './users-cms-page.component.scss' ]
+})
+export class UsersCmsPageComponent extends MetaWebContentHelper {
+
+    constructor(
+        titleService: Title,
+        metaService: Meta,
+    ) {
+        super(titleService, metaService, AllCmsWebpages.USERS);
+    };
+}
