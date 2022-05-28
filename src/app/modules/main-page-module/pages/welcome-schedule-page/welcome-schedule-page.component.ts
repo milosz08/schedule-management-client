f988a57825a8b71c77bb5c572be5340463e5040d
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-05-28 23:10:58 +0200

    initialised welcome-schedule-page ng page component

---
+++ src/app/modules/main-page-module/pages/welcome-schedule-page/welcome-schedule-page.component.ts
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2022 by MILOSZ GILGA <https://miloszgilga.pl> <https://github.com/Milosz08>
+ * Silesian University of Technology | Politechnika Śląska
+ *
+ * File name | Nazwa pliku: welcome-schedule-page.component.ts
+ * Last modified | Ostatnia modyfikacja: 24/05/2022, 23:13
+ * Project name | Nazwa Projektu: angular-po-schedule-management-client
+ *
+ * Klient | Client: <https://github.com/Milosz08/Angular_PO_Schedule_Management_Client>
+ * Serwer | Server: <https://github.com/Milosz08/ASP.NET_PO_Schedule_Management_Server>
+ *
+ * Client for the ASP.NET Core application to manage schedule for sample university. Written with the Angular Framework
+ * for generating dynamic web applications. Project for the teaching course "Objected Oriented Programming".
+ *
+ * Klient dla aplikacji ASP.NET Core służącej do zarządzania planem zajęć uczelni. Napisany przy użyciu frameworka
+ * Angular do generowania dynamicznych aplikacji webowych. Projekt wykonany na zajęcia "Programowanie
+ * Obiektowe".
+ */
+
+import { Component } from '@angular/core';
+
+//----------------------------------------------------------------------------------------------------------------------
+
+/**
+ * Strona wyświetlająca się przy załadowaniu strony planu zajęć (początkowa strona).
+ */
+
+@Component({
+    selector: 'app-welcome-schedule-page',
+    templateUrl: './welcome-schedule-page.component.html',
+    styleUrls: [ './welcome-schedule-page.component.css' ]
+})
+export class WelcomeSchedulePageComponent {
+}
