6931e8f57275b782dd21b9abf0b2b7fd6978e665
Miłosz Gilga <gilgamilosz451@gmail.com>
2022-06-01 01:38:41 +0200

    change css file to scss

--- src/app/modules/main-page-module/pages/welcome-schedule-page/welcome-schedule-page.component.ts
+++ src/app/modules/main-page-module/pages/welcome-schedule-page/welcome-schedule-page.component.ts
@@ -28,7 +28,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-welcome-schedule-page',
     templateUrl: './welcome-schedule-page.component.html',
-    styleUrls: [ './welcome-schedule-page.component.css' ]
+    styleUrls: [ './welcome-schedule-page.component.scss' ]
 })
 export class WelcomeSchedulePageComponent {
 }
