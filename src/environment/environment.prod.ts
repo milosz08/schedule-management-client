6983ef0cc610ebc41505ab67f253e090dbee00a0
Milosz08 <gilgamilosz451@gmail.com>
2024-02-21 13:04:49 +0100

    append prod and dev config to main.ts

---
+++ src/environment/environment.prod.ts
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2024 by Miłosz Gilga <https://miloszgilga.pl>
+ * Silesian University of Technology
+ */
+
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.schedule.miloszgilga.pl',
+};
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
+/*
+ * Copyright (c) 2024 by Miłosz Gilga <https://miloszgilga.pl>
+ * Silesian University of Technology
+ */
+import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
+import { environment } from './environment/environment';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
   .catch(err => console.error(err));
