2d88487a64d3b6ad920ea9dbd2ccf506a3771583
Milosz08 <gilgamilosz451@gmail.com>
2024-02-24 23:31:23 +0100

    add content to main header component

--- src/app/root-module/components/main-header/main-header.component.ts
+++ src/app/root-module/components/main-header/main-header.component.ts
@@ -2,11 +2,24 @@
  * Copyright (c) 2024 by Miłosz Gilga <https://miloszgilga.pl>
  * Silesian University of Technology
  */
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-main-header',
   templateUrl: './main-header.component.html',
   styleUrl: './main-header.component.scss',
 })
-export class MainHeaderComponent {}
+export class MainHeaderComponent {
+  isHamburgerOpen = false;
+
+  @HostListener('window:resize')
+  onResize() {
+    if (window.innerWidth > 1090 && this.isHamburgerOpen) {
+      this.isHamburgerOpen = false;
+    }
+  }
+
+  handleHamburgerToggle(): void {
+    this.isHamburgerOpen = !this.isHamburgerOpen;
+  }
+}
