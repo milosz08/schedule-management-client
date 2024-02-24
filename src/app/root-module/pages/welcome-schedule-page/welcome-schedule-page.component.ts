25719b4d4c78c607fcca0a1a70eb3c90a8a5509d
Milosz08 <gilgamilosz451@gmail.com>
2024-02-24 22:31:58 +0100

    add welcome schedule page component

---
+++ src/app/root-module/pages/welcome-schedule-page/welcome-schedule-page.component.ts
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2024 by Miłosz Gilga <https://miloszgilga.pl>
+ * Silesian University of Technology
+ */
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-schedule-page',
+  templateUrl: './welcome-schedule-page.component.html',
+  styleUrl: './welcome-schedule-page.component.scss',
+})
+export class WelcomeSchedulePageComponent {}
