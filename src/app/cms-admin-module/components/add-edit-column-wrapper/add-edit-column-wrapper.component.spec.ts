3bccff2b4604b51068f14ef65be711ba60fb07b5
Milosz08 <gilgamilosz451@gmail.com>
2024-03-17 00:12:06 +0100

    add missing provider declaration in unit test

--- src/app/cms-admin-module/components/add-edit-column-wrapper/add-edit-column-wrapper.component.spec.ts
+++ src/app/cms-admin-module/components/add-edit-column-wrapper/add-edit-column-wrapper.component.spec.ts
@@ -5,6 +5,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppModule } from '~/app.module';
 import { CmsAdminModule } from '~/cms-admin-module/cms-admin.module';
+import { AddEditContentService } from '~/cms-admin-module/services/add-edit-content/add-edit-content.service';
 import { AddEditColumnWrapperComponent } from './add-edit-column-wrapper.component';
 
 describe('AddEditColumnWrapperComponent', () => {
@@ -14,6 +15,7 @@ describe('AddEditColumnWrapperComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppModule, CmsAdminModule],
+      providers: [AddEditContentService],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AddEditColumnWrapperComponent);
